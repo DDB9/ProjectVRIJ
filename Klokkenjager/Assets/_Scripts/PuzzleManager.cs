f02d55a0bda00fb838f34bbffaa4638aac8f854c
Daan de Bruijn <ddebruijn9@gmail.com>
2019-05-09 23:18:38 +0200

    Finished Solar System Puzzle 1
    Alsjeblief Kaerel

--- Klokkenjager/Assets/_Scripts/PuzzleManager.cs
+++ Klokkenjager/Assets/_Scripts/PuzzleManager.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class PuzzleManager : MonoBehaviour
 {
     void OnTriggerEnter(Collider other) {
-        if (this.name == "Planet 1 Collider" && other.name == "Planet 1") PuzzleOne.planetOneInPlace = true; Debug.Log("Check");
-        if (this.name == "Planet 2 Collider" && other.name == "Planet 2") PuzzleOne.planetTwoInPlace = true;Debug.Log("Check");
-        if (this.name == "Planet 3 Collider" && other.name == "Planet 3") PuzzleOne.planetThreeInPlace = true;Debug.Log("Check");
-        if (this.name == "Planet 4 Collider" && other.name == "Planet 4") PuzzleOne.planetFourInPlace = true;Debug.Log("Check");
+        if (this.name == "Planet 1 Collider" && other.name == "Planet 1") PuzzleOne.planetOneInPlace = true;
+        if (this.name == "Planet 2 Collider" && other.name == "Planet 2") PuzzleOne.planetTwoInPlace = true;
+        if (this.name == "Planet 3 Collider" && other.name == "Planet 3") PuzzleOne.planetThreeInPlace = true;
+        if (this.name == "Planet 4 Collider" && other.name == "Planet 4") PuzzleOne.planetFourInPlace = true;
     }
 }
